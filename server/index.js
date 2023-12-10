83d20795bf248237052797a925ec4e68311eb67a
Currydev <kahiki@hotmail.de>
2023-12-10 14:35:14 +0100

    update: restructure startable via node

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ require("dotenv").config(); // Ensure environment variables are loaded
 const express = require("express");
 const cors = require("cors");
 const sqlite3 = require('sqlite3').verbose();
-
+const path = require('path');
 const app = express();
+
 
 // CORS options for cross-origin allowance
 var corsOptions = {
@@ -15,6 +16,7 @@ app.use(cors(corsOptions));
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Connect to SQLite database
 let db = new sqlite3.Database('./mydb.sqlite3', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
@@ -25,8 +27,8 @@ let db = new sqlite3.Database('./mydb.sqlite3', sqlite3.OPEN_READWRITE | sqlite3
 });
 
 // Simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the application." });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Additional routes (adjust as per your requirements)
@@ -65,6 +67,39 @@ const PORT = process.env.PORT || 8080; // Changed from NODE_DOCKER_PORT to PORT 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+// Close the database connection when the server is stopped
+process.on('SIGINT', () => {
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Closed the database connection.');
+    process.exit(0);
+  });
+});
+
+// Create tables if they do not exist
+db.serialize(() => {
+  // Create a 'friends' table
+  db.run(`CREATE TABLE IF NOT EXISTS friends (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT,
+    status TEXT
+  )`);
+
+  // Create an 'orders' table
+  db.run(`CREATE TABLE IF NOT EXISTS orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_date DATE,
+    order_amount REAL,
+    customer_id INTEGER,
+    FOREIGN KEY(customer_id) REFERENCES friends(id)
+  )`);
+});
+
+// The rest of your Express app setup goes here...
 
 // Close the database connection when the server is stopped
 process.on('SIGINT', () => {
